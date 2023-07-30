465ffb547847686d42a7bd4d497ad53eaec2086d
Nitesh <aryanitesh25@gmail.com>
2023-07-31 02:57:51 +0530

    Added search tool, youtube, and math tool the bot

--- main.py
+++ main.py
@@ -3,7 +3,51 @@ from textbase.message import Message
 from textbase import models
 import os
 from typing import List
+from langchain.tools import BaseTool, StructuredTool, Tool, tool
 
+from langchain.agents import Tool
+from langchain.agents import AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI
+from langchain.utilities import GoogleSerperAPIWrapper
+from langchain.agents import initialize_agent
+from langchain import LLMMathChain
+from pydantic import BaseModel, Field
+# from langchain.tools import YouTubeSearchTool
+# tool = YouTubeSearchTool()
+
+
+class CalculatorInput(BaseModel):
+    question: str = Field()
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+search = GoogleSerperAPIWrapper(serper_api_key="11bcf35bd1cdba54a4875e816b1b56e5a9169bf6")
+
+llm = OpenAI(openai_api_key="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", temperature=0)
+
+llm_math_chain = LLMMathChain(llm=llm, verbose=True)
+tools = [
+    Tool(
+        func=search.run,
+        name="Search",
+        description="useful for when you need to answer questions about current events"
+        # coroutine= ... <- you can specify an async method if desired as well
+    ),
+    # Tool(
+    #     func=tool.run,
+    #     name="YoutubeLink",
+    #     description="useful for finding video link for a person"
+    #     # coroutine= ... <- you can specify an async method if desired as well
+    # ),
+    Tool(
+        func=llm_math_chain.run,
+        name="Calculator",
+        description="useful for when you need to answer questions about math",
+        args_schema=CalculatorInput
+        # coroutine= ... <- you can specify an async method if desired as well
+    )
+]
+agent_chain = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
 # Load your OpenAI API key
 models.OpenAI.api_key = "YOUR_API_KEY"
 # or from environment variable:
@@ -29,10 +73,6 @@ def on_message(message_history: List[Message], state: dict = None):
         state["counter"] += 1
 
     # # Generate GPT-3.5 Turbo response
-    bot_response = models.OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
-        message_history=message_history,
-        model="gpt-3.5-turbo",
-    )
+    bot_response = agent_chain.run(message_history[-1].content)
 
     return bot_response, state
